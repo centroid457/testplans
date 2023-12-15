5aa13e3616f44a1ffb80eff9c8b1345cd710bc7e
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-15 10:54:23 +0300

    try add blanc structure

--- ps_qcd/main.py
+++ ps_qcd/main.py
@@ -3,9 +3,80 @@ from typing import *
 
 
 # =====================================================================================================================
-Type_Dict = Dict[str, Optional[str]]
-Type_Path = Union[str, pathlib.Path]
-Type_Value = Union[str, NoReturn, None]
+
+
+# =====================================================================================================================
+class TestCaseStep:
+    DESCRIPTION: str = None
+    RESULT: Optional[bool] = None
+    DETAILS: Dict[str, Any] = {}
+
+    @classmethod
+    @property
+    def name(cls):
+        return cls.__name__
+
+    def startup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def run(self):
+        pass
+
+    def add_details(self, details: Dict[str, Any]):
+        pass
+
+
+# =====================================================================================================================
+class TestCase:
+    DESCRIPTION: str = None
+    RESULT: Optional[bool] = None
+    DETAILS: List[TestCaseStep] = []
+
+    @classmethod
+    @property
+    def name(cls):
+        return cls.__name__
+
+    def startup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def run(self):
+        pass
+
+    def add_details(self, details: Dict[str, Any]):
+        pass
+
+
+# =====================================================================================================================
+class ResultTp:
+    DESCRIPTION: str = None
+    DETAILS: List[TestCase] = []
+
+    @classmethod
+    @property
+    def name(cls):
+        return cls.__name__
+
+    def startup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def run(self):
+        pass
+
+    def add_result(self, tc_result: TestCase):
+        pass
+
+    def check_summary_result(self):
+        pass
 
 
 # =====================================================================================================================
