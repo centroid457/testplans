72d9e2e7b9b8de9436ea2299ef446b0f0784cc4b
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-18 15:43:20 +0300

    try create manager-2

--- ps_qcd/base.py
+++ ps_qcd/base.py
@@ -4,9 +4,6 @@ import abc
 
 # =====================================================================================================================
 class _Base:
-    details: Dict
-    result: bool
-
     DUT: Any = None
     DESCRIPTION: str = ""
     PROGRESS: int = 0
@@ -28,20 +25,7 @@ class _Base:
         self.PROGRESS = 100
 
     def dump_results(self):
-        if TestCase in self.__class__.__mro__:
-            print(f"{self.name}: result={self.result}")
-            for name, value in self.details.items():
-                print(f"\t|{name}: {value}")
-                for name2, value2 in value.details.items():
-                    print(f"\t\t|{name2}: {value2}")
-
-        elif TestPlan in self.__class__.__mro__:
-            print("=" * 80)
-            for tc, tc_object in self.details.items():
-                if tc_object:
-                    # print(f"{tc_object.name}:result={tc_object.result}")
-                    tc_object.dump_results()
-            print("="*80)
+        pass
 
 
 # =====================================================================================================================
@@ -68,6 +52,13 @@ class TestCase(_Base, abc.ABC):
 
     def add_details(self, details: Dict[str, Any]) -> None:
         self.details.update(details)
+
+    def dump_results(self):
+        print(f"{self.name}: result={self.result}")
+        for name, value in self.details.items():
+            print(f"\t|{name}: {value}")
+            for name2, value2 in value.details.items():
+                print(f"\t\t|{name2}: {value2}")
 
     # -----------------------------------------------------------------------------------------------------------------
     @abc.abstractmethod
@@ -106,14 +97,14 @@ class TestCase(_Base, abc.ABC):
 
 # =====================================================================================================================
 class TestPlan(_Base):
-    details: Dict[Type[TestCase], Union[bool, TestCase]] = {
+    tcs: Dict[Type[TestCase], Union[bool, TestCase]] = {
         # TC1: True,
         # TC2: True
     }
 
     @property
     def result(self) -> bool:
-        for detail in self.details.values():
+        for detail in self.tcs.values():
             if detail in [False, None]:
                 continue
             if not detail.result:
@@ -125,40 +116,65 @@ class TestPlan(_Base):
             return
 
         if self.startup():
-            for detail, start in self.details.items():
+            for detail, start in self.tcs.items():
                 if start:
-                    self.details[detail] = detail(self.DUT)
-                    self.details[detail].run()
-                    if self.details[detail].STOP_IF_FALSE_RESULT and not self.details[detail].result:
+                    self.tcs[detail] = detail(self.DUT)
+                    self.tcs[detail].run()
+                    if self.tcs[detail].STOP_IF_FALSE_RESULT and not self.tcs[detail].result:
                         break
         self.teardown()
+
+    def dump_results(self):
+        print("=" * 80)
+        for tc, tc_object in self.tcs.items():
+            if tc_object:
+                # print(f"{tc_object.name}:result={tc_object.result}")
+                tc_object.dump_results()
+        print("="*80)
 
     # -----------------------------------------------------------------------------------------------------------------
 
 
 # =====================================================================================================================
-class ManagerTs(abc.ABC):
-    ITEMS: Dict[TestPlan, Any] = {
-        # PsQcd_Tp(1): None,
-        # PsQcd_Tp(2): None,
+class ManagerTp(abc.ABC):
+    TPs: Dict[TestPlan, Any] = {
+        # TpDut1: None,
+        # TpDut2: None,
     }
 
     def __init__(self):
-        self.items_generate()
-        self.items_check_present()
+        self.tps_generate()
 
     @abc.abstractmethod
-    def items_generate(self) -> None:
+    def tps_generate(self) -> None:
         pass
 
-    @abc.abstractmethod
-    def items_check_present(self) -> None:
-        pass
+    def tps_presented(self) -> List[TestPlan]:
+        result = []
+        for tp in self.TPs:
+            if tp.DUT.check_present():
+                result.append(tp)
+        return result
 
-    def run(self):
-        for item in self.ITEMS:
-            # item.
-            pass
+    def run(self) -> None:
+        if not self.tps_presented():
+            return
 
+        for tc in self.tps_presented()[0].tcs:
+            if tc.PARALLEL:
+                for tp in self.tps_presented():
+                    if tp.
+
+
+
+
+
+            for detail, start in self.tcs.items():
+                if start:
+                    self.tcs[detail] = detail(self.DUT)
+                    self.tcs[detail].run()
+                    if self.tcs[detail].STOP_IF_FALSE_RESULT and not self.tcs[detail].result:
+                        break
+        self.teardown()
 
 # =====================================================================================================================
