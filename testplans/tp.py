39edbf93349392bbcc3462806685fa402558bbdb
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-25 10:10:53 +0300

    add Exx__TcsPathNotExists

--- testplans/tp.py
+++ testplans/tp.py
@@ -17,6 +17,10 @@ from importlib import import_module
 
 
 # =====================================================================================================================
+class Exx__TcsPathNotExists(Exception):
+    pass
+
+
 class Exx__TcItemNotFound(Exception):
     pass
 
@@ -65,6 +69,11 @@ class TpMultyDutBase(QThread):
         self.DIRPATH_TCS: Path = Path(self.DIRPATH_TCS)
         # self.DIRPATH_DEVS: Path = Path(self.DIRPATH_DEVS)
 
+        if not self.DIRPATH_TCS.exists():
+            msg = f"[ERROR] not found path {self.DIRPATH_TCS.name=}"
+            print(msg)
+            raise Exx__TcsPathNotExists(msg)
+
         self.reinit()
 
         self.signal__tp_start.connect(self.run)
@@ -74,38 +83,35 @@ class TpMultyDutBase(QThread):
         # TCS --------------------------------------------------------------
         tcs = tcs or dict(self.TCS)
         self.TCS = {}
-        if self.DIRPATH_TCS.exists():
-            for item, using in tcs.items():
-                # print(dir(TESTCASES))
-                if isinstance(item, str):   # filename
-                    # tc_cls = import_module(item, "TESTCASES").TestCase    # not working!
-                    # tc_cls = getattr(TESTCASES, item).TestCase      # not working
-                    tc_cls = import_module(f"{self.DIRPATH_TCS.name}.{item}").TestCase
-                    if not tc_cls:
-                        msg = f"[ERROR] file not found[{item=}] in /{self.DIRPATH_TCS.name}/"
-                        raise Exx__TcItemNotFound(msg)
-                    tc_cls.NAME = item
-                elif isinstance(type(item), type) and issubclass(item, TestCaseBase):
-                    tc_cls = item
-                    # msg = f"[ERROR] DONT USE IT!"
-                    # raise Exception(msg)
-                else:
-                    msg = f"[ERROR] type is inconvenient [{item=}]"
-                    raise Exx__TcItemType(msg)
 
-                tc_cls.SKIP = not using
-                self.TCS.update({tc_cls: using})
+        for item, using in tcs.items():
+            # print(dir(TESTCASES))
+            if isinstance(item, str):   # filename
+                # tc_cls = import_module(item, "TESTCASES").TestCase    # not working!
+                # tc_cls = getattr(TESTCASES, item).TestCase      # not working
+                tc_cls = import_module(f"{self.DIRPATH_TCS.name}.{item}").TestCase
+                if not tc_cls:
+                    msg = f"[ERROR] file not found[{item=}] in /{self.DIRPATH_TCS.name}/"
+                    raise Exx__TcItemNotFound(msg)
+                tc_cls.NAME = item
+            elif isinstance(type(item), type) and issubclass(item, TestCaseBase):
+                tc_cls = item
+                # msg = f"[ERROR] DONT USE IT!"
+                # raise Exception(msg)
+            else:
+                msg = f"[ERROR] type is inconvenient [{item=}]"
+                raise Exx__TcItemType(msg)
 
-                settings_filepath = self.DIRPATH_TCS.joinpath(f"{item}.json")
-                if settings_filepath.exists():
-                    # print(f"{settings_filepath=} EXISTS")
-                    tc_cls.SETTINGS = PrivateJson(_filepath=settings_filepath)
-                else:
-                    print(f"{settings_filepath=} NOT_EXISTS")
-                    pass
-        else:
-            msg = f"[ERROR] not found path {self.DIRPATH_TCS=}"
-            print(msg)
+            tc_cls.SKIP = not using
+            self.TCS.update({tc_cls: using})
+
+            settings_filepath = self.DIRPATH_TCS.joinpath(f"{item}.json")
+            if settings_filepath.exists():
+                # print(f"{settings_filepath=} EXISTS")
+                tc_cls.SETTINGS = PrivateJson(_filepath=settings_filepath)
+            else:
+                print(f"{settings_filepath=} NOT_EXISTS")
+                pass
 
         # print(f"{tc_cls.SETTINGS=}")
 
