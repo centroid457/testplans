e981b7bc7ee228e245aacc5d07aeece83d8db086
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-25 14:40:54 +0300

    zero

--- TESTCASES/example_tc1_direct.py
+++ TESTCASES/example_tc1_direct.py
@@ -1,11 +1,14 @@
 import time
 from testplans import TestCaseBase
 
+import DEVICES
 
 class TestCase(TestCaseBase):
     ACYNC = False
     DESCRIPTION = "direct1"
     SETTINGS = {}   # DONT DELETE!
+
+    device1: DEVICES.dut_example1.Device    # APPLY any if need!
 
     def run_wrapped(self) -> bool:
         time.sleep(0.2)
