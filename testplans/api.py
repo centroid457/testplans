cad277c2b17028a0afb4c3ded371fb22c4721ff0
Andrei Starichenko <a.starichenko@element-t.ru>
2024-02-13 12:50:09 +0300

    apply new version serverTemplates for post_duplicate_for_get

--- testplans/api.py
+++ testplans/api.py
@@ -7,12 +7,6 @@ from server_templates import ServerAiohttpBase, decorator__log_request_response,
 
 # =====================================================================================================================
 class TpApi(ServerAiohttpBase):
-    # CLIENT_URL_BASE: str = f"http://starichenko"
-    # CLIENT_ROUTE__POST_RESULTS: str = "start"
-
-    CLIENT_URL_BASE: str = f"http://192.168.74.20:8080"    # LIDIA
-    CLIENT_ROUTE__POST_RESULTS: str = "results"             # LIDIA
-
     async def response_get__(self, request) -> web.Response:
         # --------------------------
         progress = 0
@@ -24,25 +18,6 @@ class TpApi(ServerAiohttpBase):
         # RESPONSE --------------------------------------------------
         page_name = "API_INDEX"
         html = self.html_create(name=page_name, data=html_block, redirect_time=2, request=request)
-        return web.Response(text=html, content_type='text/html')
-
-    @decorator__log_request_response
-    async def response_get__start(self, request) -> web.Response:
-        # ObjectInfo(request.headers).print()
-        self.data.signal__tp_start.emit()
-
-        # RESPONSE --------------------------------------------------
-        page_name = "START"
-        html = self.html_create(name=page_name, redirect_time=1, request=request)
-        return web.Response(text=html, content_type='text/html')
-
-    @decorator__log_request_response
-    async def response_get__stop(self, request) -> web.Response:
-        self.data.signal__tp_stop.emit()
-
-        # RESPONSE --------------------------------------------------
-        page_name = "STOP"
-        html = self.html_create(name=page_name, redirect_time=1, request=request)
         return web.Response(text=html, content_type='text/html')
 
     @decorator__log_request_response
@@ -61,15 +36,6 @@ class TpApi(ServerAiohttpBase):
         # RESPONSE --------------------------------------------------
         response = web.json_response(data={})
         return response
-
-    @decorator__log_request_response
-    async def response_get___reset_duts_sn(self, request) -> web.Response:
-        self.data._signal__tp_reset_duts_sn.emit()
-
-        # RESPONSE --------------------------------------------------
-        page_name = "RESET_DUTS_SN"
-        html = self.html_create(name=page_name, redirect_time=1, request=request)
-        return web.Response(text=html, content_type='text/html')
 
     @decorator__log_request_response
     async def response_post___reset_duts_sn(self, request) -> web.Response:
