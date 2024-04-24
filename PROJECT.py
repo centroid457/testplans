37e2b0a6180d29232be22c0ef28efa9a9b2a23dd
Andrei Starichenko <centroid@mail.ru>
2024-04-24 12:55:23 +0300

    ready v0.2.2

--- PROJECT.py
+++ PROJECT.py
@@ -93,7 +93,7 @@ designed to apply testplan for several DUTs
     ]
 
     # HISTORY -----------------------------------------------
-    VERSION: Tuple[int, int, int] = (0, 2, 1)
+    VERSION: Tuple[int, int, int] = (0, 2, 2)
     TODO: List[str] = [
         "add meta for settings in tcs, it is better then applying in manually in TP!",
         "close all (api_server+tpThreads) on GUI close!",
@@ -105,7 +105,7 @@ designed to apply testplan for several DUTs
         "NEED TESTS!!! TC+TP"
     ]
     NEWS: List[str] = [
-        "[API] fix working with FastApi",
+        "[GUI] fix exx on TV tapping",
     ]
 
     # FINALIZE -----------------------------------------------
