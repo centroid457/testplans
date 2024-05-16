aece1303298d01dc7661afe35a79c751c7debe2e
Andrei Starichenko <centroid@mail.ru>
2024-05-16 10:53:58 +0300

    ready v0.2.15

--- PROJECT.py
+++ PROJECT.py
@@ -93,7 +93,7 @@ designed to apply testplan for several DUTs
     ]
 
     # HISTORY -----------------------------------------------
-    VERSION: Tuple[int, int, int] = (0, 2, 14)
+    VERSION: Tuple[int, int, int] = (0, 2, 15)
     TODO: List[str] = [
         "add meta for settings in tcs, it is better then applying in manually in TP!",
         "close all (api_server+tpThreads) on GUI close!",
@@ -105,7 +105,7 @@ designed to apply testplan for several DUTs
         "NEED TESTS!!! TC+TP"
     ]
     NEWS: List[str] = [
-        "[TC] fix info_pretty",
+        "[TC] fix/add cls_clear on run_cls",
     ]
 
     # FINALIZE -----------------------------------------------
